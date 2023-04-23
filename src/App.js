c0cf71dce91c6f50231d8b70b763794c131b572d
ranjan1992 <verma.ranjan62@gmail.com>
2023-04-23 09:46:32 +0530

    Sending Icons from Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from './logo.svg'
-import './App.css'
 import Icon from './components/Icon'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-function App() {
+import 'bootstrap/dist/css/bootstrap.css'
+
+import { Card, CardBody, Container, Button, Col, Row } from 'reactstrap'
+
+const App = () => {
+  const itemArray = new Array(9).fill('empty')
+  const [isCross, setIsCross] = useState(false)
+  const [winMessage, setWinMessage] = useState('')
+  
+// Reloading the Game
+  const reloadGame = () => {}
+
+//Check the Winner
+  const checkWinner = () => {}
+
+//change the Item if there is no Item present  
+  const changeItem = () => {}
   return (
     <div className="App">
       <Icon />
