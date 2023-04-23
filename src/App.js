1bad8e6e7bcd8eab16147b8d461d425c7d47b32a
ranjan1992 <verma.ranjan62@gmail.com>
2023-04-23 09:57:24 +0530

    Setup Layout for TicTacToe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Icon from './components/Icon'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import { useState } from 'react'
 
 import { Card, CardBody, Container, Button, Col, Row } from 'reactstrap'
 
@@ -10,19 +11,32 @@ const App = () => {
   const itemArray = new Array(9).fill('empty')
   const [isCross, setIsCross] = useState(false)
   const [winMessage, setWinMessage] = useState('')
-  
-// Reloading the Game
+
+  // Reloading the Game
   const reloadGame = () => {}
 
-//Check the Winner
+  //Check the Winner
   const checkWinner = () => {}
 
-//change the Item if there is no Item present  
+  //change the Item if there is no Item present
   const changeItem = () => {}
   return (
-    <div className="App">
-      <Icon />
-    </div>
+    <Container className="p-5">
+      <ToastContainer position="bottom-center" />
+      <Row>
+        <Col md={6} className="offset-md-3">
+          <div className="grid">
+            {itemArray.map((item, index) => (
+              <Card>
+                <CardBody className="box">
+                  <Icon name={item} />
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
